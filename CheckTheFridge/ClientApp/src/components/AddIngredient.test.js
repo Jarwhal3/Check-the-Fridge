c77b9258859c7adf46d633297adf33dc09dd1e6b
Naya <44099724+nayamairena@users.noreply.github.com>
2023-03-16 22:11:51 -0700

    attempt at tests

---
+++ CheckTheFridge/ClientApp/src/components/AddIngredient.test.js
@@ -0,0 +1,8 @@
+﻿import { render, screen } from "@testing-library/react";
+import AddIngredient from "./IngredientForm";
+
+test('on render', () => {
+    render(<AddIngredient />);
+
+    screen.debug();
+})
