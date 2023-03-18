0617b91c7177719b49f5dd988ef472eab64d3ec5
Naya <44099724+nayamairena@users.noreply.github.com>
2023-03-17 21:05:30 -0700

    Change edit and delete buttons to be in ingredient list component

--- CheckTheFridge/ClientApp/src/components/Ingredient.js
+++ CheckTheFridge/ClientApp/src/components/Ingredient.js
@@ -3,27 +3,15 @@ import { ListGroup, ListGroupItem } from 'reactstrap';
 import { Button, Row, Col, Container } from 'reactstrap';
 
 
-const Ingredient = ({ ingredient, onDelete, onEdit }) => {
+const Ingredient = ({ ingredient }) => {
     return (
-        <ListGroup className="task">
-            <ListGroupItem>
-                <div>
-                    <p className="ingredientName">Ingredient: {ingredient.name}</p>
-                    <p className="ingredientDesc"><span className="textBold">Description:</span> {ingredient.description}
-                    </p>
-                    <p className="ingredientID"><span className="textBold">ID:</span> {ingredient.id}
-                    </p>
-                    <p className="ingredientQuan"><span className="textBold">Quantity:</span> {ingredient.quantity}
-                    </p>
-                </div>
-                <Container>
-                    <Row>
-                        <Col><Button className="w-100" onClick={() => onEdit(ingredient.id)}>Edit</Button></Col>
-                        <Col><Button className="w-100" onClick={() => onDelete(ingredient.id)}>Delete</Button></Col>
-                    </Row>
-                </Container>
-                </ListGroupItem>
-            </ListGroup>
+        <div>
+            <p className="ingredientName">Ingredient: {ingredient.name}</p>
+            <p className="ingredientDesc">Description: {ingredient.description}</p>
+            <p className="ingredientID">ID: {ingredient.id}</p>
+            <p className="ingredientQuan">Quantity: {ingredient.quantity}</p>
+        </div>
     )
 }
+
 export default Ingredient;
--- CheckTheFridge/ClientApp/src/components/IngredientList.js
+++ CheckTheFridge/ClientApp/src/components/IngredientList.js
@@ -1,19 +1,29 @@
 import Ingredient from './Ingredient';
-import  Scrollbars  from 'react-custom-scrollbars-2';
+import Scrollbars from 'react-custom-scrollbars-2';
+import { Button, Row, Col, Container } from 'reactstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+
 
 
 
 const IngredientList = ({ ingredientList, onDelete, onEdit }) => {
     return (
         <Scrollbars style={{ width: "100%", height: 500 }}>
-        <div>
             {
                 ingredientList.map((ingredient) => (
-                    <Ingredient key={ingredient.id} ingredient={ingredient} onDelete={onDelete} onEdit={onEdit} />
-                ))
+                    <ListGroup>
+                        <ListGroupItem>
+                            <Container>
+                                <Ingredient key={ingredient.id} ingredient={ingredient}/>
+                                <Row>
+                                    <Col><Button className="w-100" onClick={() => onEdit(ingredient.id)}>Edit</Button></Col>
+                                    <Col><Button className="w-100" onClick={() => onDelete(ingredient.id)}>Delete</Button></Col>
+                                </Row>
+                            </Container>
+                        </ListGroupItem>
+                    </ListGroup>))
             }
-            </div>
-            </Scrollbars>
-    )
+        </Scrollbars>)
 }
+
 export default IngredientList;
